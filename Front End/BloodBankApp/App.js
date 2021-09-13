0f97673b8d09f2fcb6a67c7a5f2dd354440ab95b
arhamabeer <arhamabeerahmed@gmail.com>
2021-09-13 23:21:14 +0500

    creating new react native app...

---
+++ Front End/BloodBankApp/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello Peter...</Text>
+    </View>
+  );
+}
+
+export default App;
