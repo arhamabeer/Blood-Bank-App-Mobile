109f7cae089945a47cff825933767886d9bff3e5
arhamabeer <arhamabeerahmed@gmail.com>
2021-09-14 01:35:40 +0500

    setting backgroud and initializing src files...

--- Front End/BloodBankApp/App.js
+++ Front End/BloodBankApp/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  Text,
+  View,
+  ImageBackground,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+const BgImg = require('./src/assets/bg-blood.jpg');
 
 function App() {
   return (
     <View>
-      <Text>Hello Peter...</Text>
+      <ImageBackground source={BgImg} style={styles.bgImg} resizeMode="cover">
+        <Text>Hello Peter...</Text>
+      </ImageBackground>
     </View>
   );
 }
+const styles = StyleSheet.create({npm install @react-navigation/native-stack
+  bgImg: {
+    width: Dimensions.get('screen').width,
+    height: '100%',
+  },
+});
 
 export default App;
